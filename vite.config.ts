b294dcad78c19332b9be2c6c7e314b4e5079584b
刘志伟 <2055572879@qq.com>
2024-06-19 19:05:59 +0800

    feat chunk

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,33 @@ export default defineConfig({
       tsDecorators: true,
     }),
   ],
+  build: {
+    outDir: "../api/static",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[name].[hash].js",
+        entryFileNames: "js/[name].[hash].js",
+        manualChunks(id) {
+          // 处理 pnpm 的依赖路径
+          if (id.includes("node_modules")) {
+            const directories = id.toString().split("node_modules/");
+            if (directories.length > 2) {
+              return directories[2].split("/")[0].toString();
+            }
+            return directories[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
